e4402e659d47959fc2c25acd43f85ecd82c05967
Chris Ineson <chrisineson@gmail.com>
2020-04-21 23:27:43 +0100

    Widen banner & add logo

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,8 +7,9 @@ class Banner extends Component {
   render() {        
     return (
       <div>
-        <img alt="" style={{ width: "100%", height: "100px"}} src="images/banner.png"/>
-        <div style={{ position: "absolute", top: "10px", left: "10px", color: "white", fontSize: "1.5em"}}>
+        <img alt="" style={{ width: "100%", height: "180px"}} src="images/banner.png"/>
+        <img alt="" style={{ position: "absolute", top: "10px", left: "10px" }} src="images/logo2.png"/>
+        <div style={{ position: "absolute", top: "110px", left: "10px", color: "white", fontSize: "1.5em"}}>
           Super-cute crocheted accessories,<br/>hand-made with <span role="img" aria-label="hearts">💕</span> since 2020
         </div>
       </div>
