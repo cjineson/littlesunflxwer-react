8195799bf6e91aaf91d8a34686320b8a219752c4
Chris Ineson <chrisineson@gmail.com>
2022-09-19 10:28:29 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.0866958655089561b8a97ebb6607e473.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c54a83ff1f4584881943c310f1bf228",
+    "url": "/littlesunflxwer-react/index.html"
+  },
+  {
+    "revision": "ede1140fbbaac105a242",
+    "url": "/littlesunflxwer-react/static/css/main.18264b2c.chunk.css"
+  },
+  {
+    "revision": "ec020907c54f179d77d4",
+    "url": "/littlesunflxwer-react/static/js/2.ba9a4c03.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/littlesunflxwer-react/static/js/2.ba9a4c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede1140fbbaac105a242",
+    "url": "/littlesunflxwer-react/static/js/main.0023ce10.chunk.js"
+  },
+  {
+    "revision": "31563362e3c8b88d9a69",
+    "url": "/littlesunflxwer-react/static/js/runtime-main.37a486a8.js"
+  },
+  {
+    "revision": "50fc884e5d848451bc2853cd6436054c",
+    "url": "/littlesunflxwer-react/static/media/background.50fc884e.jpg"
+  },
+  {
+    "revision": "3127570d76224b1819c2b7f7cda61dca",
+    "url": "/littlesunflxwer-react/static/media/bee.3127570d.jpg"
+  },
+  {
+    "revision": "bc02a0dc9b4db3992d162b9736131e05",
+    "url": "/littlesunflxwer-react/static/media/flower.bc02a0dc.jpg"
+  },
+  {
+    "revision": "684a05189793a3860e7b6b97b9af00e8",
+    "url": "/littlesunflxwer-react/static/media/logo.684a0518.jpg"
+  },
+  {
+    "revision": "a035cdd7ef6ee3ab9a959e51427cc2cb",
+    "url": "/littlesunflxwer-react/static/media/sad-sunflower.a035cdd7.png"
+  }
+]);
